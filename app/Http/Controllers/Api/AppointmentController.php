d6bbdc6a1b9e41bd646ace52675ff8382558bf28
JP <riquelme_2112@hotmail.com>
2020-02-19 21:13:37 -0600

    add Appointment model to api controller

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreAppointment;
+use App\Appointment;
 use Auth;
 
 class AppointmentController extends Controller
